ec9e4678d971032f48ca68298734597b54e5278f
Alex Schwantes <2829978+alexschwantes@users.noreply.github.com>
2024-02-02 12:50:17 +1000

    update _handle_new_items_added to use tuples

--- custom_components/google_keep_sync/const.py
+++ custom_components/google_keep_sync/const.py
@@ -1,3 +1,4 @@
 """Constants for the Google Keep Sync integration."""
 
 DOMAIN = "google_keep_sync"
+EVENT_NEW_ITEM = "google_keep_sync_new_item"
--- custom_components/google_keep_sync/coordinator.py
+++ custom_components/google_keep_sync/coordinator.py
@@ -1,6 +1,8 @@
 """DataUpdateCoordinator for the Google Keep Sync component."""
 
 import logging
+from collections import namedtuple
+from collections.abc import Callable
 from datetime import timedelta
 
 from gkeepapi.node import List as GKeepList
@@ -8,9 +10,14 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
 from .api import GoogleKeepAPI
-from .const import DOMAIN
+from .const import DOMAIN, EVENT_NEW_ITEM
 
 _LOGGER = logging.getLogger(__name__)
+TodoItem = namedtuple("TodoItem", ["summary", "checked"])
+TodoList = namedtuple("TodoList", ["name", "items"])
+TodoItemData = namedtuple(
+    "TodoItemData", ["item_name", "item_id", "item_checked", "list_name", "list_id"]
+)
 
 
 class GoogleKeepSyncCoordinator(DataUpdateCoordinator[list[GKeepList]]):
@@ -41,60 +48,73 @@ class GoogleKeepSyncCoordinator(DataUpdateCoordinator[list[GKeepList]]):
             result = await self.api.async_sync_data(lists_to_sync)
             # save lists after syncing
             updated_lists = await self._parse_gkeep_data_dict()
-            # compare both list for changes
-            await self._check_gkeep_lists_changes(original_lists, updated_lists)
+            # compare both list for changes, and fire event for changes
+            await self._handle_new_items_added(
+                original_lists,
+                updated_lists,
+                self.config_entry.data.get("list_prefix", ""),
+                lambda item_data: self.hass.bus.async_fire(EVENT_NEW_ITEM, item_data),
+            )
 
             return result
         except Exception as error:
             raise UpdateFailed(f"Error communicating with API: {error}") from error
 
-    async def _parse_gkeep_data_dict(self) -> dict[str, dict[str, dict[str, str]]]:
-        """Parse unchecked gkeep api data to a dictionary."""
-        todo_lists = {}
+    async def _parse_gkeep_data_dict(self) -> dict[str, TodoList]:
+        """Parse unchecked gkeep data to a dictionary, with the list id as the key."""
+        all_keep_lists = {}
 
         # for each list
-        for glist in self.data or []:
+        for keep_list in self.data or []:
             # get all the unchecked items only
             items = {
-                item.id: {"summary": item.text, "checked": item.checked}
-                for item in glist.items
+                item.id: TodoItem(summary=item.text, checked=item.checked)
+                for item in keep_list.items
                 if not item.checked
             }
-            todo_lists[glist.id] = {"name": glist.title, "items": items}
-        return todo_lists
+            all_keep_lists[keep_list.id] = TodoList(name=keep_list.title, items=items)
+        return all_keep_lists
 
-    async def _check_gkeep_lists_changes(self, original_lists, updated_lists) -> None:
-        """Compare original_lists and updated_lists lists.
+    async def _handle_new_items_added(
+        self,
+        original_lists: dict[str, TodoList],
+        updated_lists: dict[str, TodoList],
+        list_prefix: str,
+        on_new_item: Callable[[TodoItemData], None],
+    ) -> None:
+        """Compare original and updated lists to find new TodoItems.
 
-        Report on any new TodoItem's that have been added to any
-        lists in updated_lists that are not in original_lists.
+        For each new TodoItem found, call the provided on_new_item callback.
+
+        :param original_lists: The original todo list data.
+        :param updated_lists: The updated todo list data.
+        # :param on_new_item: Callback function to execute for each new item found.
         """
         # for each list
         for updated_list_id, upldated_list in updated_lists.items():
             if updated_list_id not in original_lists:
-                _LOGGER.debug(
-                    "Found new list not in original: %s", upldated_list["name"]
-                )
+                _LOGGER.debug("Found new list not in original: %s", upldated_list.name)
                 continue
 
             # for each todo item in the list
-            for upldated_list_item_id, upldated_list_item in upldated_list[
-                "items"
-            ].items():
-                # if todo is not in original list, then it is new
-                if (
-                    upldated_list_item_id
-                    not in original_lists[updated_list_id]["items"]
-                ):
-                    list_prefix = self.config_entry.data.get("list_prefix", "")
-                    data = {
-                        "item_name": upldated_list_item["summary"],
-                        "item_id": upldated_list_item_id,
-                        "item_checked": upldated_list_item["checked"],
-                        "list_name": (f"{list_prefix} " if list_prefix else "")
-                        + upldated_list["name"],
-                        "list_id": updated_list_id,
-                    }
+            upldated_list_item: TodoItem
+            for (
+                upldated_list_item_id,
+                upldated_list_item,
+            ) in upldated_list.items.items():
+                # get original list with updated_list_id
+                original_list = original_lists[updated_list_id]
 
-                    _LOGGER.debug("Found new TodoItem: %s", data)
-                    self.hass.bus.async_fire("google_keep_sync_new_item", data)
+                # if todo is not in original list, then it is new
+                if upldated_list_item_id not in original_list.items:
+                    item_data = TodoItemData(
+                        item_name=upldated_list_item.summary,
+                        item_id=upldated_list_item_id,
+                        item_checked=upldated_list_item.checked,
+                        list_name=(f"{list_prefix} " if list_prefix else "")
+                        + upldated_list.name,
+                        list_id=updated_list_id,
+                    )
+
+                    _LOGGER.debug("Found new TodoItem: %s", item_data)
+                    on_new_item(item_data)
